b5843e12120d84c56d9aab8b4ee4b075e97b3393
Fernanda Ochoa <monc_sip@hotmail.com>
2016-02-09 11:46:11 -0600

    feat: add new classes

--- src/cursohuetamo/TestOr.java
+++ src/cursohuetamo/TestOr.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cursohuetamo;
 
 /**
@@ -10,5 +6,22 @@ package cursohuetamo;
  * @author monsh
  */
 public class TestOr {
-    
+
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 10;
+        boolean test = false;
+
+        //demostrate |
+        test = (i > 10) || (j++ > 9);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(test);
+
+        //demostrate |
+        test = (i > 10) | (j++ > 9);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(test);
+    }
 }
