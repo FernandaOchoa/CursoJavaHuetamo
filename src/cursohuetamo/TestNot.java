be4846a899ca06b257b63c62c75a81233d5cbcbe
Fernanda Ochoa <monc_sip@hotmail.com>
2016-02-09 12:00:19 -0600

    feat: add class Not

---
+++ src/cursohuetamo/TestNot.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursohuetamo;
+
+/**
+ *
+ * @author monsh
+ */
+public class TestNot {
+
+    public static void main(String[] args) {
+        boolean v1 = true;
+        boolean v2 = false;
+
+        System.out.println(!v1);
+        System.out.println(!v2);
+    }
+
+}
